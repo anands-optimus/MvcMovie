cef341483acfd8120a1d433629695da9659544a2
anands-optimus <anand.shukla@optimusinfo.com>
2019-05-15 14:53:26 +0530

    added calendar action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,9 @@ namespace MvcMovie.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        publid IActionResult Calendar()
+        {
+            return View();
+        }
     }
 }
